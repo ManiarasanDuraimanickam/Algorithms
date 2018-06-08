a504405ac019b602bc438b1e6542ce617e4a2a30
Maniarasan D <maniarasand@iLinkmultitech.com>
2018-06-08 18:05:00 +0530

    Modified third approach

--- src/java/main/algo/tryout/matrix/MatrixLayerRotation.java
+++ src/java/main/algo/tryout/matrix/MatrixLayerRotation.java
@@ -3,19 +3,15 @@ package algo.tryout.matrix;
 public class MatrixLayerRotation {
 
 	public static void main(String[] args) {
-		//int[][] matrix = new int[][] { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 },
-		//		{ 17, 18, 19, 20 } };
+		// int[][] matrix = new int[][] { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11,
+		// 12 }, { 13, 14, 15, 16 },
+		// { 17, 18, 19, 20 } };
 
-		int[][] sortedMatrix = new int[][] { { 1, 2, 3, 4, 5, 6, 7, 8 },
-				{ 9, 10, 11, 12, 13, 14, 15, 16 },
-				{ 17, 18, 19, 20, 21, 22, 23, 24 },
-				{ 25, 26, 27, 28, 29, 30, 31, 32 },
-				{ 33, 34, 35, 36, 37, 38, 39, 40 },
-				{ 41, 42, 43, 44, 45, 46, 47, 48 },
-				{ 49, 50, 51, 52, 53, 54, 55, 56 },
-				{ 57, 58, 59, 60, 61, 62, 63, 64 },
-				{ 65, 66, 67, 68, 69, 70, 71, 72 },
-				{ 73, 74, 75, 76, 77, 78, 79, 80 } };
+		int[][] sortedMatrix = new int[][] { { 1, 2, 3, 4, 5, 6, 7, 8 }, { 9, 10, 11, 12, 13, 14, 15, 16 },
+				{ 17, 18, 19, 20, 21, 22, 23, 24 }, { 25, 26, 27, 28, 29, 30, 31, 32 },
+				{ 33, 34, 35, 36, 37, 38, 39, 40 }, { 41, 42, 43, 44, 45, 46, 47, 48 },
+				{ 49, 50, 51, 52, 53, 54, 55, 56 }, { 57, 58, 59, 60, 61, 62, 63, 64 },
+				{ 65, 66, 67, 68, 69, 70, 71, 72 }, { 73, 74, 75, 76, 77, 78, 79, 80 } };
 
 		// int[][] sortedMatrix = matrix.clone();
 		int r = 40, len = 8, heg = 10;
@@ -57,15 +53,26 @@ public class MatrixLayerRotation {
 			while (lIcursor < heg) {
 				lNewPosition = newPosition + lIcursor;
 				if (lNewPosition > heg) {
-					ljNewPosition++;
+					ljNewPosition = lNewPosition - heg;
 					lNewPosition = heg;
+				}
+				if (ljNewPosition > len) {
+					lNewPosition = lNewPosition - ((ljNewPosition + i) - len);
+					ljNewPosition = len;
 				}
 				newSortedMatrix[lNewPosition][ljNewPosition] = sortedMatrix[lIcursor][lJcursor];
 
 				rNewPosition = rIcursor - newPosition;
+				
+				if(rNewPosition>rIcursor) {
+					rIcursor=i;
+				}
 				if (rNewPosition < i) {
-					rjNewPosition--;
+					rjNewPosition=rNewPosition+len;
 					rNewPosition = i;
+				}
+				if(rjNewPosition>len) {
+					rNewPosition=rNewPosition + ((rNewPosition - i) - heg);
 				}
 				newSortedMatrix[rNewPosition][rjNewPosition] = sortedMatrix[rIcursor][rJcursor];
 				lIcursor++;
